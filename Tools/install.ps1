6d3f613394f7df53c27b6e3fdc63d6075399a5bd
Dan Mohl <dmohl@yahoo.com>
2012-07-07 21:06:29 -0500

    Updated the NuGet package to automatically call Add-BindingRedirect.

--- Tools/install.ps1
+++ Tools/install.ps1
@@ -2,4 +2,5 @@ param($installPath, $toolsPath, $package, $project)
     $project.Object.References.Add("*System.Web");
     $project.Object.References.Add("*System.ComponentModel.DataAnnotations");
     $project.Object.References.Add("*System.Web.Mvc");
+	Add-BindingRedirect $project.Name
     
--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-c:\nuget\nuget.exe pack c:\git\FsUnit.MvcSample\FsUnit.MvcSample.nuspec -b c:\git\FsUnit.MvcSample -o c:\git\FsUnit.MvcSample
+c:\nuget\nuget.exe pack .\FsUnit.MvcSample.nuspec
 md c:\nuget\FsUnit.MvcSample\
-copy c:\git\FsUnit.MvcSample\*.nupkg c:\nuget\FsUnit.MvcSample\ /Y
+copy *.nupkg c:\nuget\FsUnit.MvcSample\ /Y
 pause
